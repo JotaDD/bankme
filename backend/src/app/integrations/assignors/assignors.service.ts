7b1c81262ba7a3acec31081ab3e4113705688689
JotaDD <joaoeduardogomes@gmail.com>
2024-05-24 20:15:31 -0300

    feat: add assignor crud service

--- backend/src/app/integrations/assignors/assignors.service.ts
+++ backend/src/app/integrations/assignors/assignors.service.ts
@@ -1,26 +1,34 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/database/prisma/prisma.service';
 import { CreateAssignorDto } from './dto/create-assignor.dto';
 import { UpdateAssignorDto } from './dto/update-assignor.dto';
 
 @Injectable()
 export class AssignorsService {
+  constructor(private prisma: PrismaService) {}
+
   create(createAssignorDto: CreateAssignorDto) {
-    return 'This action adds a new assignor';
+    return this.prisma.assignor.create({
+      data: createAssignorDto,
+    });
   }
 
   findAll() {
-    return `This action returns all assignors`;
+    return this.prisma.assignor.findMany();
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} assignor`;
+  findOne(id: string) {
+    return this.prisma.assignor.findUnique({ where: { id } });
   }
 
-  update(id: number, updateAssignorDto: UpdateAssignorDto) {
-    return `This action updates a #${id} assignor`;
+  update(id: string, updateAssignorDto: UpdateAssignorDto) {
+    return this.prisma.assignor.update({
+      where: { id },
+      data: updateAssignorDto,
+    });
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} assignor`;
+  remove(id: string) {
+    return this.prisma.assignor.delete({ where: { id } });
   }
 }
