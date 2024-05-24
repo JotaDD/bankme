f17283912f65782dbebcfb8fe1293f769e2f5f3b
JotaDD <joaoeduardogomes@gmail.com>
2024-05-24 20:14:19 -0300

    feat: add validations and swagger properties

--- backend/src/app/integrations/assignors/dto/create-assignor.dto.ts
+++ backend/src/app/integrations/assignors/dto/create-assignor.dto.ts
@@ -1,15 +1,31 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { Transform } from 'class-transformer';
+import { IsEmail, IsNotEmpty, MaxLength } from 'class-validator';
 
 export class CreateAssignorDto {
-  @ApiProperty()
+  @ApiProperty({ example: '12345678901' })
+  @IsNotEmpty()
+  @MaxLength(30)
+  @Transform(({ value }) => value.replace(/\D/g, ''))
   document: string;
 
-  @ApiProperty()
+  @ApiProperty({ example: 'johnny@test.com' })
+  @IsNotEmpty()
+  @IsEmail({}, { message: 'email must be valid' })
+  @MaxLength(140)
   email: string;
 
-  @ApiProperty()
+  @ApiProperty({
+    example: '11999998888',
+    description: 'Only numbers, the api will remove all non-numeric characters',
+  })
+  @IsNotEmpty()
+  @MaxLength(20)
+  @Transform(({ value }) => value.replace(/\D/g, ''))
   phone: string;
 
-  @ApiProperty()
+  @ApiProperty({ example: 'Johnny Test' })
+  @IsNotEmpty()
+  @MaxLength(140)
   name: string;
 }
