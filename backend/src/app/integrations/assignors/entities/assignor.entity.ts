ba51233f3a66040d42f1d964d6fb25ac9d5c7d7d
JotaDD <joaoeduardogomes@gmail.com>
2024-05-24 20:14:39 -0300

    feat: add swagger properties

--- backend/src/app/integrations/assignors/entities/assignor.entity.ts
+++ backend/src/app/integrations/assignors/entities/assignor.entity.ts
@@ -1,9 +1,31 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { UUID } from 'crypto';
 import { Assignor } from 'src/interfaces';
 
 export class AssignorEntity implements Assignor {
-  id: string;
+  @ApiProperty({
+    nullable: false,
+    required: true,
+    type: 'string',
+    format: 'uuid',
+    example: '550e8400-e29b-41d4-a716-446655440000',
+  })
+  id: UUID;
+
+  @ApiProperty({ nullable: false, required: true, example: '12345678901' })
   document: string;
+
+  @ApiProperty({ nullable: false, required: true, example: 'johnny@test.com' })
   email: string;
+
+  @ApiProperty({
+    nullable: false,
+    required: true,
+    example: '11999998888',
+    description: 'Only numbers, the api will remove all non-numeric characters',
+  })
   phone: string;
+
+  @ApiProperty({ nullable: false, required: true, example: 'Johnny Test' })
   name: string;
 }
