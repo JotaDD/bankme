8ef635a7bc13967cada4b20e7c9313d0f223b25e
JotaDD <joaoeduardogomes@gmail.com>
2024-05-24 20:16:00 -0300

    feat: add swagger properties

--- backend/src/app/integrations/assignors/assignors.controller.ts
+++ backend/src/app/integrations/assignors/assignors.controller.ts
@@ -1,45 +1,53 @@
 import {
-  Controller,
-  Get,
-  Post,
   Body,
-  Patch,
-  Param,
+  Controller,
   Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
 } from '@nestjs/common';
+import { ApiCreatedResponse, ApiOkResponse, ApiTags } from '@nestjs/swagger';
 import { AssignorsService } from './assignors.service';
 import { CreateAssignorDto } from './dto/create-assignor.dto';
 import { UpdateAssignorDto } from './dto/update-assignor.dto';
+import { AssignorEntity } from './entities/assignor.entity';
 
 @Controller()
+@ApiTags('Assignors')
 export class AssignorsController {
   constructor(private readonly assignorsService: AssignorsService) {}
 
   @Post()
+  @ApiCreatedResponse({ status: 201, type: AssignorEntity })
   create(@Body() createAssignorDto: CreateAssignorDto) {
     return this.assignorsService.create(createAssignorDto);
   }
 
   @Get()
+  @ApiOkResponse({ isArray: true, type: AssignorEntity })
   findAll() {
     return this.assignorsService.findAll();
   }
 
   @Get(':id')
+  @ApiOkResponse({ type: AssignorEntity })
   findOne(@Param('id') id: string) {
-    return this.assignorsService.findOne(+id);
+    return this.assignorsService.findOne(id);
   }
 
   @Patch(':id')
+  @ApiOkResponse({ type: AssignorEntity })
   update(
     @Param('id') id: string,
     @Body() updateAssignorDto: UpdateAssignorDto,
   ) {
-    return this.assignorsService.update(+id, updateAssignorDto);
+    return this.assignorsService.update(id, updateAssignorDto);
   }
 
   @Delete(':id')
+  @ApiOkResponse({ type: AssignorEntity })
   remove(@Param('id') id: string) {
-    return this.assignorsService.remove(+id);
+    return this.assignorsService.remove(id);
   }
 }
