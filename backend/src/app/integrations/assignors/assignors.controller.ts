e709cab0ed610d76fd6f41de840c588fbd2a2058
JotaDD <joaoeduardogomes@gmail.com>
2024-05-25 04:07:50 -0300

    refactor add async methods

--- backend/src/app/integrations/assignors/assignors.controller.ts
+++ backend/src/app/integrations/assignors/assignors.controller.ts
@@ -8,6 +8,7 @@ import {
   Post,
 } from '@nestjs/common';
 import { ApiCreatedResponse, ApiOkResponse, ApiTags } from '@nestjs/swagger';
+import { Assignor } from 'src/interfaces';
 import { AssignorsService } from './assignors.service';
 import { CreateAssignorDto } from './dto/create-assignor.dto';
 import { UpdateAssignorDto } from './dto/update-assignor.dto';
@@ -20,34 +21,36 @@ export class AssignorsController {
 
   @Post()
   @ApiCreatedResponse({ status: 201, type: AssignorEntity })
-  create(@Body() createAssignorDto: CreateAssignorDto) {
+  async create(
+    @Body() createAssignorDto: CreateAssignorDto,
+  ): Promise<Assignor> {
     return this.assignorsService.create(createAssignorDto);
   }
 
   @Get()
   @ApiOkResponse({ isArray: true, type: AssignorEntity })
-  findAll() {
+  async findAll(): Promise<Assignor[]> {
     return this.assignorsService.findAll();
   }
 
   @Get(':id')
   @ApiOkResponse({ type: AssignorEntity })
-  findOne(@Param('id') id: string) {
+  async findOne(@Param('id') id: string): Promise<Assignor> {
     return this.assignorsService.findOne(id);
   }
 
   @Patch(':id')
   @ApiOkResponse({ type: AssignorEntity })
-  update(
+  async update(
     @Param('id') id: string,
     @Body() updateAssignorDto: UpdateAssignorDto,
-  ) {
+  ): Promise<Assignor> {
     return this.assignorsService.update(id, updateAssignorDto);
   }
 
   @Delete(':id')
   @ApiOkResponse({ type: AssignorEntity })
-  remove(@Param('id') id: string) {
+  async remove(@Param('id') id: string): Promise<Assignor> {
     return this.assignorsService.remove(id);
   }
 }
