a112cbfc17927f9097bd77fbd5ab42af1f53ed2e
JotaDD <joaoeduardogomes@gmail.com>
2024-05-25 04:08:37 -0300

    test: add assignor mocks

---
+++ backend/src/app/integrations/assignors/tests/mocks/index.ts
@@ -0,0 +1,73 @@
+import { AssignorsService } from '../../assignors.service';
+import { CreateAssignorDto } from '../../dto/create-assignor.dto';
+import { UpdateAssignorDto } from '../../dto/update-assignor.dto';
+import { AssignorEntity } from '../../entities/assignor.entity';
+import { v4 as uuid } from 'uuid';
+
+export const mockAssignorList: AssignorEntity[] = [
+  {
+    id: uuid() as string,
+    document: '12345678901',
+    email: 'johnny@tes.com',
+    phone: '11999998888',
+    name: 'Johnny Test',
+  },
+  {
+    id: uuid() as string,
+    document: '12345678902',
+    email: 'mary@test.com',
+    phone: '11999998889',
+    name: 'Mary Test',
+  },
+  {
+    id: uuid() as string,
+    document: '12345678903',
+    email: 'susan@test.com',
+    phone: '11999998890',
+    name: 'Susan Test',
+  },
+];
+
+export const mockAssignorsService = {
+  provide: AssignorsService,
+  useValue: {
+    create: jest.fn().mockImplementation((dto: CreateAssignorDto) => ({
+      id: uuid(),
+      ...dto,
+    })),
+    findAll: jest.fn().mockResolvedValue(mockAssignorList),
+    findOne: jest
+      .fn()
+      .mockImplementation((id: string) =>
+        Promise.resolve(
+          mockAssignorList.find((assignor) => assignor.id === id),
+        ),
+      ),
+    update: jest
+      .fn()
+      .mockImplementation((id: string, dto: UpdateAssignorDto) =>
+        Promise.resolve({ id, ...dto }),
+      ),
+    remove: jest
+      .fn()
+      .mockImplementation((id: string) =>
+        Promise.resolve(
+          mockAssignorList.find((assignor) => assignor.id === id),
+        ),
+      ),
+  },
+};
+
+export const mockCreateAssignorDto: CreateAssignorDto = {
+  document: '12345678901',
+  email: 'johnny@test.com',
+  phone: '11999998888',
+  name: 'Johnny Test',
+};
+
+export const mockUpdateAssignorDto: UpdateAssignorDto = {
+  document: '12345678902',
+  email: 'update@test.com',
+  phone: '11999998889',
+  name: 'Update Test',
+};
