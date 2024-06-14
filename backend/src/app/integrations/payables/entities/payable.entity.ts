12d3cdae386a86f0d90ab8963430b58523dbd88c
JotaDD <joaoeduardogomes@gmail.com>
2024-06-14 17:34:15 -0300

    feat(payable): add swagger properties to entity

--- backend/src/app/integrations/payables/entities/payable.entity.ts
+++ backend/src/app/integrations/payables/entities/payable.entity.ts
@@ -1,9 +1,34 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { UUID } from 'crypto';
 import { Payable } from 'src/interfaces';
 
 export class PayableEntity implements Payable {
+  @ApiProperty({
+    nullable: false,
+    required: true,
+    type: 'string',
+    format: 'uuid',
+    example: '550e8400-e29b-41d4-a716-446655440000',
+  })
   id?: UUID;
+
+  @ApiProperty({
+    nullable: false,
+    required: true,
+    type: Number,
+    example: 190.55,
+  })
   value: number;
+
+  @ApiProperty({ nullable: false, required: true, example: 'johnny@test.com' })
   emissionDate: Date;
+
+  @ApiProperty({
+    nullable: false,
+    required: true,
+    type: 'string',
+    format: 'uuid',
+    example: '550e8400-e29b-41d4-a716-446655440000',
+  })
   assignorId: UUID;
 }
