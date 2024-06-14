8f8484969c836a9e81f744c0f41a478d08484a5f
JotaDD <joaoeduardogomes@gmail.com>
2024-06-14 17:33:49 -0300

    refactor(payable): change assignor id type to return a string

--- backend/src/app/integrations/payables/dto/create-payable.dto.ts
+++ backend/src/app/integrations/payables/dto/create-payable.dto.ts
@@ -1,6 +1,5 @@
 import { ApiProperty } from '@nestjs/swagger';
 import { IsNotEmpty } from 'class-validator';
-import { UUID } from 'crypto';
 
 export class CreatePayableDto {
   @ApiProperty({ example: 100.0 })
@@ -13,5 +12,5 @@ export class CreatePayableDto {
 
   @ApiProperty({ example: '123e4567-e89b-12d3-a456-426614174000' })
   @IsNotEmpty()
-  assignorId: UUID;
+  assignorId: string;
 }
