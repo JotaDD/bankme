a2d2dec10f92576017939763eec7fb3595038554
JotaDD <joaoeduardogomes@gmail.com>
2024-06-18 14:05:14 -0300

    feat(helpers): add message helpers

---
+++ backend/src/app/helpers/messages.helper.ts
@@ -0,0 +1,8 @@
+export enum MessageHelper {
+  EMAIL_VALID = 'O campo precisa ser um email precisa ser válido.',
+  ONLY_NUMBER = 'O campo precisar conter apenas números.',
+  MAX_TWENTY = 'O campo deve ter no máximo 20 caracteres',
+  MAX_THIRTY = 'O campo deve ter no máximo 30 caracteres',
+  MAX_ONE_FORTY = 'O campo deve ter no máximo 140 caracteres',
+  NOT_EMPTY = 'O campo não pode ser vazio.',
+}
