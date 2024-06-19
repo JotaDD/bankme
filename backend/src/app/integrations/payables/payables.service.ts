fc691a35fc330b0585f9a848d57bcb297f6e0fec
JotaDD <joaoeduardogomes@gmail.com>
2024-06-19 16:42:05 -0300

    feat(payable): add payable service

--- backend/src/app/integrations/payables/payables.service.ts
+++ backend/src/app/integrations/payables/payables.service.ts
@@ -1,33 +1,110 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, HttpStatus, Injectable } from '@nestjs/common';
+import { PayableDto } from './dto/payable.dto';
 
 import { PrismaService } from 'src/database/prisma/prisma.service';
-import { UpdatePayableDto } from './dto/update-payable.dto';
 import { CreatePayableDto } from './dto/create-payable.dto';
+import { UpdatePayableDto } from './dto/update-payable.dto';
+import { PayableNotFoundException } from './exceptions/payable-not-found.exception';
+import { AssignorsService } from '../assignors/assignors.service';
+import { AssignorNotFoundException } from '../assignors/exceptions';
+import { async } from 'rxjs';
 
 @Injectable()
 export class PayablesService {
-  constructor(private prisma: PrismaService) {}
+  constructor(
+    private prisma: PrismaService,
+    private assignorService: AssignorsService
+  ) { }
 
-  async create(createPayableDto: CreatePayableDto) {
-    return await this.prisma.payable.create({ data: createPayableDto });
+  async create(createPayableDto: CreatePayableDto): Promise<PayableDto> {
+    const assignor = await this.assignorService.findOne(createPayableDto.assignorId)
+    if (!assignor) {
+      throw new AssignorNotFoundException()
+    }
+    createPayableDto.assignorId = assignor.id
+    createPayableDto.emissionDate = createPayableDto.emissionDate ? new Date(createPayableDto.emissionDate) : new Date()
+
+    return await this.prisma.payable.create({
+      data: createPayableDto
+    })
   }
 
-  async findAll() {
-    return await this.prisma.payable.findMany();
-  }
 
-  async findOne(id: string) {
-    return await this.prisma.payable.findUnique({ where: { id } });
-  }
-
-  async update(id: string, updatePayableDto: UpdatePayableDto) {
-    return await this.prisma.payable.update({
-      where: { id: id },
-      data: updatePayableDto,
+  async findAll(): Promise<PayableDto[]> {
+    return await this.prisma.payable.findMany({
+      include: {
+        assignor: {
+          select: {
+            name: true,
+            document: true,
+            email: true,
+            phone: true,
+          }
+        }
+      }
     });
   }
 
-  async remove(id: string) {
-    await this.prisma.payable.delete({ where: { id } });
+  async findOne(id: string): Promise<PayableDto> {
+    const payable = await this.prisma.payable.findUnique({
+      where: { id }, include: {
+        assignor: {
+          select: {
+            name: true,
+            document: true,
+            email: true,
+            phone: true,
+          }
+        },
+      }
+    })
+    if (!payable) {
+      throw new PayableNotFoundException();
+    }
+    return payable
   }
+
+  async update(id: string, updatePayableDto: UpdatePayableDto) {
+    const payable = await this.prisma.payable.findUnique({ where: { id } })
+
+    const assignorToBeUpdated = await this.prisma.assignor.findUnique({
+      where: { id: updatePayableDto.assignorId }
+    })
+
+    if (!payable) {
+      throw new PayableNotFoundException()
+    }
+
+    if (!assignorToBeUpdated) {
+      throw new AssignorNotFoundException()
+    }
+
+    return await this.prisma.payable.update({
+      where: { id: payable.id },
+      data: updatePayableDto,
+      include: {
+        assignor: {
+          select: {
+            name: true,
+            document: true,
+            email: true,
+            phone: true,
+          }
+        }
+      }
+    });
+  }
+  
+  async remove(id: string) {
+    try {
+      await this.prisma.payable.delete({ where: { id } });
+      return {
+        "statusCode": HttpStatus.OK,
+        "message": "Recebível removido com sucesso!"
+      }
+    } catch (error) {
+      throw new PayableNotFoundException();
+    }
+  }
+
 }
