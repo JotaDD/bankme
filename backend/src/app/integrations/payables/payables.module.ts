29d39e02b4456d251b4365589a3246fcef90b6a7
JotaDD <joaoeduardogomes@gmail.com>
2024-06-19 16:43:02 -0300

    feat(payable): add payable module

--- backend/src/app/integrations/payables/payables.module.ts
+++ backend/src/app/integrations/payables/payables.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { PayablesService } from './payables.service';
 import { PayablesController } from './payables.controller';
+import { AssignorsService } from '../assignors/assignors.service';
 
 @Module({
   controllers: [PayablesController],
-  providers: [PayablesService],
+  providers: [PayablesService, AssignorsService],
 })
 export class PayablesModule {}
